8b08b67dbee2e99b69788e530549778a40e7f781
adiiwinarnoo <adiwinarno860@gmail.com>
2023-05-23 14:14:09 +0700

    created splash screen

--- app/src/main/java/com/example/jepretajitu/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/jepretajitu/view/activity/SplashScreenActivity.kt
@@ -1,15 +1,25 @@
 package com.example.jepretajitu.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
 import com.example.jepretajitu.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
     lateinit var binding : ActivitySplashScreenBinding
+    var timer : Int = 5000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, timer.toLong())
     }
 }
