b2740d0e9646a693392c8ec0f50c78a5451f6717
adiiwinarnoo <adiwinarno860@gmail.com>
2023-06-05 10:13:03 +0700

    created layout upload katalog and menu utama for fotographer

--- app/src/main/java/com/example/jepretajitu/utils/Constant.kt
+++ app/src/main/java/com/example/jepretajitu/utils/Constant.kt
@@ -7,5 +7,9 @@ class Constant {
         const val EMPTY_PASSWORD = "password tidak boleh kosong"
         const val CHECK_CONNECTION = "cek koneksi anda"
         const val SUCCESS_LOGIN = "Login Berhasil"
+
+        // saving id level to share pref
+        const val ADD_ID_LEVEL = "add_id_level"
+        const val ADD_NAME = "addName"
     }
 }
---
+++ app/src/main/java/com/example/jepretajitu/view/activity/fotographer/AddFotoActivity.kt
@@ -0,0 +1,24 @@
+package com.example.jepretajitu.view.activity.fotographer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jepretajitu.R
+import com.example.jepretajitu.databinding.ActivityAddFotoBinding
+import com.example.jepretajitu.databinding.ActivityLoginBinding
+import com.example.jepretajitu.utils.Constant
+import com.example.jepretajitu.utils.SharedPrefences
+
+class AddFotoActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAddFotoBinding
+    lateinit var sharedPrefences: SharedPrefences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddFotoBinding.inflate(layoutInflater)
+        sharedPrefences = SharedPrefences(this)
+        setContentView(binding.root)
+
+        binding.tvFotograpWelcome.text = "${R.string.selamat_datang_kembali} ${sharedPrefences.getStringData(Constant.ADD_NAME)}"
+    }
+}
